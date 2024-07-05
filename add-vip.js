2d142df684153bd96f69d43b987a1c7340c01808
SergioDiscordJava <sergiovazquez8821@icloud.com>
2024-07-05 17:50:54 -0600

    Update add-vip.js

--- add-vip.js
+++ add-vip.js
@@ -5,5 +5,5 @@ let rand = Math.random().toString(16).slice(2,10) + Math.random().toString(16).s
 if(process.argv[2]) rand = process.argv[2] + rand
 if(process.argv[3]) rand = process.argv[3] + rand
 
-fs.appendFileSync("./vip.txt", `\n# ${process.argv[2]}\n${sha256(rand)} {"cooldownMs": 500, "perms": "vip" }`)
+fs.appendFileSync("./vip.txt", `\n# ${process.argv[2]}\n${sha256(rand)} {"cooldownMs": 100, "perms": "vip" }`)
 console.log("Key:", rand)
